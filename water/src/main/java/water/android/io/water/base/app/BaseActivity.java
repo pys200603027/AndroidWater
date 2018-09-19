088462a7e833ea98f7750b47512db141350eab9b
pys200603027 <pys200603027@gmail.com>
2018-09-19 17:11:03 +0800

    gradle依赖管理调整

---
+++ water/src/main/java/water/android/io/water/base/app/BaseActivity.java
@@ -0,0 +1,10 @@
+package water.android.io.water.base.app;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author pys
+ */
+public class BaseActivity extends AppCompatActivity {
+
+}
---
+++ water/src/main/java/water/android/io/water/base/app/BaseFragment.java
@@ -0,0 +1,7 @@
+package water.android.io.water.base.app;
+
+import android.support.v4.app.Fragment;
+
+public class BaseFragment extends Fragment {
+
+}
